2ed409b52e816c7294e3d310d1fd85a4f3418fb2
Ilenia Taccogna <itaccogna@codearchitects.com>
2025-03-14 15:09:32 +0100

    create new angular app

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', loadComponent: () => import('./home-page/home-page.component').then(c=> c.HomepageComponent)},
+    { path: 'home-page', loadComponent: () => import('./home-page/home-page.component').then(c=> c.HomepageComponent)},
+    { path: 'chi-siamo', loadComponent: () => import('./chi-siamo/chi-siamo.component').then(c=> c.ChiSiamoComponent)},
+    { path: 'progetti', loadComponent: () => import('./progetti/progetti.component').then(c=> c.ProgettiComponent)},
+    { path: 'contatti', loadComponent: () => import('./contatti/contatti.component').then(c=> c.ContattiComponent)},
+    { path: 'card', loadComponent: () => import('./card/card.component').then(c=> c.CardComponent)},
+    { path: 'footer', loadComponent: () => import('./footer/footer.component').then(c=> c.FooterComponent)},
+
+    
+];
---
+++ src/app/chi-siamo/chi-siamo.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-chi-siamo',
+  imports: [],
+  templateUrl: './chi-siamo.component.html',
+  styleUrl: './chi-siamo.component.scss'
+})
+export class ChiSiamoComponent {
+  fototesseraZullo="/FototesseraZullo.jpg";
+  fototesseraTaccogna="/FototesseraTaccogna.jpg";
+}
