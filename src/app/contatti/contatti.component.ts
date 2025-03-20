facdf9d878bbaea780235217b959e3a8333efbcf
Federico Zullo <fzullo@codearchitects.com>
2025-03-20 12:35:25 +0100

    feat:validator in .html e nel relativo .ts

--- src/app/contatti/contatti.component.ts
+++ src/app/contatti/contatti.component.ts
@@ -1,5 +1,5 @@
 import { Component, inject } from '@angular/core';
-import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { ContattiService } from '../service/contatto.service';
 import { CommonModule } from '@angular/common';
 
@@ -17,9 +17,9 @@ export class ContattiComponent {
 
  constructor() { 
    this.contattiForm= this._formContactBuilder.nonNullable.group({
-     name:[''],
-     email:[''],
-     message:['']
+     name:['',[Validators.required, Validators.minLength(5),Validators.pattern(/^[a-zA-ZÀ-ÿ\s]+$/)]],
+     email:['',[Validators.required, Validators.email, Validators.pattern(/^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/)]],
+     message:['',[Validators.required, Validators.minLength(10)]]
    });
   }
 
